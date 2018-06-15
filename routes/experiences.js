93a7accc5662b96db389817ffbb0746ae39c5b8b
Kristoffer Ronn <roenn18@gmail.com>
2018-06-15 10:53:04 +0200

    experience search and front page search and user registration button

--- routes/experiences.js
+++ routes/experiences.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var Place = require("../models/place");
 var Experience = require("../models/experience");
+var geodist = require("geodist"); // TO CALCULATE DISTANCE BETWEEN COORDINATES
 var middleware = require("../middleware");
 
 // NODE GEOCODER CODE
@@ -17,7 +18,74 @@ var options = {
 var geocoder = NodeGeocoder(options);
 
 // EXPERIENCES SEARCH ROUTE
-
+router.post("/experiences/search", function(req, res){
+    // Save location to variable
+    var location = req.body.locationexp;
+    // Check if user typed postal code
+    if(location.length === 4 && !isNaN(location)){
+        // Add correct geolocation syntax for geocoder to find correct coordinates based on danish zip code
+        location = location + ", denmark";
+    };
+    // Use geocoder to turn location into coordinates
+    geocoder.geocode(location, function(err, data){
+        if(err || !data.length){
+            console.log(err);
+            console.log(data);
+            return res.redirect("back");
+        }
+        // Save user coordinates in temporary variables
+        var latUser = data[0].latitude;
+        var lngUser = data[0].longitude;
+        // Create temporary variable to store distances between experience and user coordinates
+        var tempDataArrBelow = [];
+        var tempDataArrAbove = [];
+        // Set above and below target
+        var distTarget = 30;
+        // Find all experiences in the database and save to temporary variable "allExperiences"
+        Experience.find({}, function(err, allExperiences){
+            if(err) {
+                console.log(err);
+            } else {
+                const monthNames = ["Januar", "Februar", "Marts", "April", "Maj", "Juni",
+                    "Juli", "August", "September", "Oktober", "November", "December"
+                ];
+                // Put experience in temporary array
+                allExperiences.forEach(function(experience){
+                    // Create object with experience inside
+                    var experienceData = experience;
+                    // Calculate distance between user coordinates and experience coordinates
+                    var dist = geodist({lat: experience.lat, lon: experience.lng}, {lat: latUser, lon: lngUser}, {unit: "km"});
+                    // Create element in the place object with the distance
+                    experienceData.distance = dist;
+                    // Set month and day parameters
+                    var dateParts = experience.start.date.split("-");
+                    var monthNumber = parseInt(dateParts[1], 10) - 1;
+                    experience.month = monthNames[monthNumber];
+                    experience.day = parseInt(dateParts[2], 10);
+                    // Push object with experience to the temporary array tempDataArr depending on distance
+                    if(dist < distTarget){
+                        tempDataArrBelow.push(experienceData);
+                    } else {
+                        tempDataArrAbove.push(experienceData);
+                    }
+                });
+                // Sort below list ascending based on the date
+                tempDataArrBelow.sort(function(a, b){
+                    var a1= Date.parse(a.start.date), b1= Date.parse(b.start.date);
+                    if(a1== b1) return 0;
+                    return a1> b1? 1: -1;
+                });
+                // Sort above list ascending based on the date
+                tempDataArrAbove.sort(function(a, b){
+                    var a1= Date.parse(a.start.date), b1= Date.parse(b.start.date);
+                    if(a1== b1) return 0;
+                    return a1> b1? 1: -1;
+                });
+                res.render("experiences/search", {experiencesBelow: tempDataArrBelow, experiencesAbove: tempDataArrAbove, searchterm: req.body.locationexp});
+            }
+        });
+    });
+});
 
 // EXPERIENCES INDEX ROUTE
 router.get("/experiences", function(req, res){
