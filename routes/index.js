bc450a3f5e72fd00f00ea1a5947c67b235260c17
Kristoffer Ronn <roenn18@gmail.com>
2018-08-01 12:54:14 +0200

    add pictures for baksminde and strandegaard

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,8 @@ router.get("/support", function(req, res){
     res.render("support");
 });
 
+// 404 ROUTE
+
 // NEW USER ROUTE
 router.get("/users/new", function(req, res){
     res.render("users/new");
