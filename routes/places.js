54dcde8e88f33be36d15502ba91c7204573d7a1d
Kristoffer Ronn <roenn18@gmail.com>
2018-08-03 15:58:49 +0200

    add services to places show page if they are active. add youtube link.

--- routes/places.js
+++ routes/places.js
@@ -147,7 +147,7 @@ router.post("/places", middleware.isLoggedIn, function(req, res){
 
 // PLACES SHOW ROUTE
 router.get("/places/:id", function(req, res){
-    Place.findById(req.params.id).populate("products").populate("experiences").exec(function(err, foundPlace){
+    Place.findById(req.params.id).populate("products").populate("experiences").populate("services").exec(function(err, foundPlace){
         if(err) {
             console.log(err);
         } else {
