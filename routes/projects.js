807861703b83726fd1bb207ccc97c02d552667c3
Kristoffer Ronn <roenn18@gmail.com>
2020-04-06 16:40:23 +0200

    minor visual fix for layout maps with white outline

--- routes/projects.js
+++ routes/projects.js
@@ -122,7 +122,7 @@ router.get("/projects/:id/layout", middleware.isLoggedIn, function(req, res){
                 var rowArray = [];
                 var distance = rowWidth;
                 // OFFSET AND CREATE NEW LINE FOR EACH ROW - NB. WORKS BECAUSE -1 CANCELS < rowCount BY 1.
-                for(i=0;i<rowCount+2;i++){
+                for(i=0;i<rowCount;i++){
                     var bufferLine1 = buffer(line, distance, {units: "meters"});
                     var rowPoints1 = lineIntersect(bufferLine1, polygon);
                     var row1 = turf.lineString([[rowPoints1.features[0].geometry.coordinates[0],rowPoints1.features[0].geometry.coordinates[1]],[rowPoints1.features[1].geometry.coordinates[0],rowPoints1.features[1].geometry.coordinates[1]]],{name: "line-0" + i });
