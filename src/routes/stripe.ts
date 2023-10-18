cd1a77ad747c93fce34bf129b519cce73c82f2f1
Birk Skyum <birk.skyum@pm.me>
2023-10-18 02:44:37 +0200

    payment method if required

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -91,6 +91,7 @@ router.post(
           },
         ],
         allow_promotion_codes: true,
+        payment_method_collection: "if_required",
         customer: customer?.valueOf().toString(),
         mode: 'subscription',
         currency: currency.valueOf().toString(),
