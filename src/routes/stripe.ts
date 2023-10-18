cfcf86520b784185862b5f3a3cd995839fa9cc3d
Birk Skyum <birk.skyum@pm.me>
2023-10-18 02:42:06 +0200

    update api version

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -52,7 +52,7 @@ export const StripeIds = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2023-10-16',
   maxNetworkRetries: 2,
 });
 
