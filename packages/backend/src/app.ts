e6d45f1581c750839ba51d76b943a7d58dbcde8c
Birk Skyum <birk.skyum@pm.me>
2024-04-23 23:48:22 +0200

    print IP/PORT

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -245,10 +245,13 @@ app.get(
 );
 app.set("trust proxy", true);
 
+const PORT = process.env.BACKEND_PORT ? parseInt(process.env.BACKEND_PORT) : 3001
+const IP = process.env.BACKEND_IP ?? "127.0.0.1"
+
 app.listen(
-	process.env.BACKEND_PORT ? parseInt(process.env.BACKEND_PORT) : 3001,
-	process.env.IP ?? "127.0.0.1",
+	PORT,
+	IP,
 	() => {
-		console.log("RegenWorks backend server has started!");
+		console.log(`RegenWorks backend server has started on ${IP}:${PORT}`);
 	},
 );
