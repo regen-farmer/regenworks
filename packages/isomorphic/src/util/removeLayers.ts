67696e32037f21f70407c2985534715fbaf5a19f
Birk Skyum <birk.skyum@pm.me>
2024-05-07 02:34:31 +0200

    mute log

--- packages/isomorphic/src/util/removeLayers.ts
+++ packages/isomorphic/src/util/removeLayers.ts
@@ -1,8 +1,6 @@
 export function removeLayers(layerNames: string[], map:maplibregl.Map) {
   for (const layerName of layerNames) {
     if (map.getSource(layerName)) {
-
-      console.log("HERE", layerName)
       map.removeLayer(layerName);
       map.removeSource(layerName);
     }
