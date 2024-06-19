ce3d21eeb77525abb75adb7a3271ddcaa1b03e99
Birk Skyum <birk.skyum@pm.me>
2024-06-19 20:42:00 +0200

    style ruler

--- packages/isomorphic/src/components/Measure.tsx
+++ packages/isomorphic/src/components/Measure.tsx
@@ -18,6 +18,7 @@ export default class MeasuresControl implements IControl {
       useGrouping: "always",
     };
     this._drawCtrl = new MapboxDraw({
+      
       displayControlsDefault: false,
       styles: [
         // ACTIVE (being drawn)
--- packages/isomorphic/src/routes/parcels/[parcelId]/index.tsx
+++ packages/isomorphic/src/routes/parcels/[parcelId]/index.tsx
@@ -99,26 +99,26 @@ export default function view() {
                 text: {
                     radialOffset:  0.9,
                     letterSpacing: 0.05,
-                    color: '#D20C0C',
+                    color: '#fff',
                     haloColor: '#fff',
                     haloWidth: 0,
                     font: 'Noto Sans Bold',
                 },
                 common: {
                     midPointRadius: 3,
-                    midPointColor: '#D20C0C',
+                    midPointColor: 'black',
                     midPointHaloRadius: 5,
                     midPointHaloColor: '#FFF',
                 },
                 areaMeasurement: {
-                    fillColor: '#D20C0C',
-                    fillOutlineColor: '#D20C0C',
-                    fillOpacity: 0.01,
+                    fillColor: 'yellow',
+                    fillOutlineColor: 'yellow',
+                    fillOpacity: 0.10,
                     lineWidth: 2,
                 },
                 lengthMeasurement: {
                     lineWidth: 2,
-                    lineColor: "#D20C0C",
+                    lineColor: "yellow",
                 },
             }
         }), "top-left");
