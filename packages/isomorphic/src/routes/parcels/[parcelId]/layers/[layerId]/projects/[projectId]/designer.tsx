247ef75fee91c7efefb371c4fc7d4c0b345c7290
Birk Skyum <birk.skyum@pm.me>
2024-08-24 05:04:09 +0200

    fix buttons

--- packages/isomorphic/src/routes/parcels/[parcelId]/layers/[layerId]/projects/[projectId]/designer.tsx
+++ packages/isomorphic/src/routes/parcels/[parcelId]/layers/[layerId]/projects/[projectId]/designer.tsx
@@ -816,7 +816,7 @@ export default function view() {
 												<button
 													// disabled={submitDisabled()}
 													type="submit"
-													class="rounded-sm p-1 m-1 btn-default"
+													class="rounded-sm p-1 m-1 btn-danger"
 													onclick={saveSystem}
 													disabled={
 														saving() ||
