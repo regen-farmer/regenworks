0fe5f11b0cab28420dfcefe68c86cb7d13d0619a
Birk Skyum <birk.skyum@pm.me>
2024-08-24 04:23:22 +0200

    typo

--- packages/isomorphic/src/components/NavBar.tsx
+++ packages/isomorphic/src/components/NavBar.tsx
@@ -52,7 +52,7 @@ export function NavBar() {
 	});
 
 	return (
-		<nav class="flex items-center h-14 border-b border-slate-600 bg-white dark:bg-customdark1f">
+		<nav class="flex items-center h-14 border-b border-slate-600 bg-white dark:bg-customdark1">
 			<div
 				id="logotype"
 				class="mx-3 cursor-pointer"
