b1888a00b63de0e5c1fcaedbd09ead3da6a724fb
Birk Skyum <birk.skyum@pm.me>
2024-08-27 01:08:59 +0200

    fix dm parameter box bg

--- packages/isomorphic/src/routes/parcels/[parcelId]/layers/[layerId]/projects/[projectId]/designer.tsx
+++ packages/isomorphic/src/routes/parcels/[parcelId]/layers/[layerId]/projects/[projectId]/designer.tsx
@@ -888,7 +888,7 @@ export { drawSystemDesign };
 
 const Parameterbox = ({ system, setSystem, logSystem }: any) => {
 	return (
-		<div class="dark:bg-background bg-white text-black right-2 top-2 p-2 rounded-md border border-zinc-300 dark:border-slate-600  dark:text-white absolute">
+		<div class="dark:bg-customdark1 bg-white text-black right-2 top-2 p-2 rounded-md border border-zinc-300 dark:border-slate-600  dark:text-white absolute">
 			<Show when={system}>
 				{/* 
               <div class='form-group'>
