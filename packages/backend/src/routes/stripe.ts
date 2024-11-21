41fe33fbdef207ccc60b5f451dde487417ad819b
Birk Skyum <birk.skyum@pm.me>
2024-11-21 22:55:49 +0100

    stripe 2024-09-30.acacia

--- packages/backend/src/routes/stripe.ts
+++ packages/backend/src/routes/stripe.ts
@@ -52,7 +52,7 @@ export const StripeIds = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2024-06-20",
+	apiVersion: "2024-09-30.acacia",
 	maxNetworkRetries: 2,
 });
 
