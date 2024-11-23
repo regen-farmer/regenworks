3c77b97031a22d7a99fd179d30e5cffc7247aca6
Birk Skyum <birk.skyum@pm.me>
2024-11-23 19:32:40 +0100

    set nitro compat date

--- packages/isomorphic/app.config.ts
+++ packages/isomorphic/app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
 	ssr: true,
+	server: {
+		compatibilityDate: '2024-11-23'
+	},
 	solid: {
 		babel: {
 			env: {
