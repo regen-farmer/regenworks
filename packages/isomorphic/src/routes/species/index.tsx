dfc741013f01d1a107950dff101de68652a0791c
Birk Skyum <birk.skyum@pm.me>
2025-03-03 09:44:01 +0100

    dark mode

--- packages/isomorphic/src/routes/species/index.tsx
+++ packages/isomorphic/src/routes/species/index.tsx
@@ -26,7 +26,7 @@ export default function view() {
 			
 			<p class="mb-4 mt-4">Count: {data()?.species?.length}</p>
 			{/* Header Row */}
-			<div class="flex font-bold bg-gray-100 border-b border-gray-300 py-2">
+			<div class="flex font-bold bg-gray-100 dark:bg-gray-700 border-b border-gray-300 dark:border-gray-600 py-2 text-gray-900 dark:text-gray-100">
 				<div class="flex-1">Common name</div>
 				<div class="flex-1">Latin</div>
 				<div class="flex-1">Precip</div>
@@ -39,7 +39,7 @@ export default function view() {
 			{/* Data Rows */}
 			<For each={data()?.species}>
 				{(species) => (
-					<div class="flex items-center border-b border-gray-200 py-2">
+					<div class="flex items-center border-b border-gray-200 dark:border-gray-600 py-2">
 						<div class="flex-1">{species.nameCommon}</div>
 						<div class="flex-1">{species.genus} {species.species}</div>
 						<div class="flex-1">
@@ -72,7 +72,7 @@ export default function view() {
 						<div class="flex-1">
 							<A
 								href={`/species/${species._id}`}
-								class="rounded-sm py-1 px-2 bg-gray-200 hover:bg-gray-300"
+								class="rounded-sm py-1 px-2 bg-gray-200 dark:bg-gray-600 text-gray-900 dark:text-gray-100 hover:bg-gray-300 dark:hover:bg-gray-500"
 							>
 								Show more
 							</A>
